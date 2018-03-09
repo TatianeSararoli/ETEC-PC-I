21d51a554e4af5a85de45423a0b109eaa908a702
Jeferson Roberto de Lima <jefersonrl@icloud.com>
2018-03-09 18:30:47 -0300

    Revisao de Conteudo

---
+++ ExemploAula/src/aula/TiposPrimitivos.java
@@ -0,0 +1,21 @@
+package aula;
+
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int idade = 25;
+		float valor;
+		valor = 1.99f;
+		boolean verdadeiroOuFalso = false;
+		verdadeiroOuFalso = true;
+		char letraA = 'A';
+		letraA = 65;
+		letraA = '\u0041';
+		byte b = 127;
+		short s = 1024;
+		long l = 1234567890;
+		double d = 100.0;
+		int var1=0, var2=1, var3=2, var4;
+	}
+}
